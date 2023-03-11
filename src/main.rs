af5acd4f825d5657252ac5fa43b4029f23bbe1b9
Djordje Pesic <djpesic@yahoo.com>
2023-03-11 03:11:55 -0800

    Add cmdline args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use bitcoin_p2p_example::config::Config;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// TCP port to connect to.
+    #[arg(short, long, default_value_t = String::from("mainnet_config.toml"))]
+    config_file: String
+}
 
 fn main() {
-    let conf = Config::new(String::from("mainnet_config.toml"));
+    let args = Args::parse();
+
+    let conf = Config::new(args.config_file);
     println!("Config {:?}", conf);
 }
